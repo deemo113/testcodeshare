d522a07dd2e3281ac441a41196d9de45f02021f7
deemo113 <deem.shaikh@gmail.com>
2019-09-04 23:13:39 -0700

    newcode

---
+++ jackpot.py
@@ -0,0 +1,14 @@
+def testjack(arr):
+    ans = False
+    num = arr[0]
+    count = 0
+    len(num)
+    for i in range(len(arr)):
+        if num == arr[i]:
+            count += 1
+    if count == len(arr):
+        ans = True
+
+    return ans
+
+print(testjack(["n","n","n","n"]))
